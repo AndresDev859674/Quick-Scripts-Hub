8363826934eb1354abb9cc8349f32c54a29c09cd
Andrew <167466992+AndresDev859674@users.noreply.github.com>
2024-11-11 14:34:48 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Quick Scripts Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "Quick Scripts Hub (Stable)", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
 local Tab = Window:MakeTab({
 	Name = "Basic",
